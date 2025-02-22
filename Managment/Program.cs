b1b6af980043edface4b138034bbbfb6b910f385
OG <o.gojayev2000@gmail.com>
2025-02-22 13:15:59 +0100

    Remove unnecessary break statement in Program.cs

--- Managment/Program.cs
+++ Managment/Program.cs
@@ -110,7 +110,6 @@ internal abstract class Program
                             {
                                 break;
                             }
-                            break;
                         }
                         break;
                     }
